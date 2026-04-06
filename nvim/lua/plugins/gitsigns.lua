416bebaae78be8877e699b28b85c9e21cc00a998
Wljmanie <wljmanie@gmail.com>
2026-04-06 09:32:08 +0200

    Added plugins

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,21 +1,20 @@
--- Adds git related signs to the gutter, as well as utilities for managing changes
 return {
-  'lewis6991/gitsigns.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim'},
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-    signs_staged = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
+	"lewis6991/gitsigns.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		signs = {
+			add = { text = "+" },
+			change = { text = "~" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+		},
+		signs_staged = {
+			add = { text = "+" },
+			change = { text = "~" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+		},
+	},
 }
